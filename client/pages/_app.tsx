7cab73a036c5ca54dcdd11adac888b0d091640e1
PLDaily <pcd12321@gmail.com>
2019-02-15 21:09:30 +0800

    feat: add global css style

--- client/pages/_app.tsx
+++ client/pages/_app.tsx
@@ -3,6 +3,8 @@ import { Provider } from 'react-redux';
 import withRedux from 'next-redux-wrapper';
 import App, { Container } from 'next/app';
 import configureStore from '../redux';
+import 'normalize.css/normalize.css';
+import '../styles/layout.scss';
 
 class MyApp extends App<any> {
   static async getInitialProps({ Component, ctx }) {
--- config/config.local.ts
+++ config/config.local.ts
@@ -1,11 +1,114 @@
 import * as withTypescript from '@zeit/next-typescript';
-import * as withSass from '@zeit/next-sass';
+import * as MiniCssExtractPlugin from 'mini-css-extract-plugin';
+import * as path from 'path';
+import * as webpack from 'webpack';
 
-const conf = withTypescript(withSass({
-  cssModules: true
-}));
+const conf = withTypescript({
+  webpack(config, options) {
+    const { dev, isServer } = options;
 
-const config = {
+    config.module.rules.push({
+      test: /\.css$/,
+      include: [
+        path.resolve(__dirname, '../client/styles'),
+        path.resolve(__dirname, '../node_modules/'),
+      ],
+      use: [
+        dev && 'extracted-loader',
+        MiniCssExtractPlugin.loader,
+        {
+          loader: 'css-loader',
+          options: {
+            modules: false,
+            url: true,
+            import: false,
+          },
+        },
+      ].filter(Boolean),
+    });
+
+    config.module.rules.push({
+      test: /\.(scss|sass)$/,
+      include: [path.resolve(__dirname, '../client/styles')],
+      use: [
+        MiniCssExtractPlugin.loader,
+        {
+          loader: 'css-loader',
+          options: {
+            modules: false,
+            url: true,
+            import: false,
+          },
+        },
+        'sass-loader',
+      ],
+    });
+
+    const cssLoader = {
+      loader: isServer ? 'css-loader/locals' : 'css-loader',
+      options: {
+        modules: true,
+        minimize: !dev,
+        sourceMap: dev,
+        camelCase: true,
+        localIdentName: '[local]_[hash:base64:5]',
+        importLoaders: 1,
+        namedExport: !isServer || undefined,
+      },
+    };
+
+    const sassLoader = { loader: 'sass-loader' };
+
+    const loaders = isServer
+      ? [cssLoader, sassLoader]
+      : [
+        dev && 'extracted-loader',
+        MiniCssExtractPlugin.loader,
+        {
+          loader: 'css-loader',
+          options: {
+            modules: true,
+            localIdentName: '[local]_[hash:base64:5]',
+            url: true,
+            import: false,
+          },
+        },
+        'sass-loader',
+      ].filter(Boolean);
+
+    options.defaultLoaders.sass = loaders;
+
+    config.module.rules.push({
+      test: /\.(scss|sass)$/,
+      use: options.defaultLoaders.sass,
+      include: [
+        path.resolve(__dirname, '../client/pages'),
+      ],
+    });
+
+    config.plugins.push(
+      new MiniCssExtractPlugin({
+        filename: dev
+          ? 'static/chunks/[name].css'
+          : 'static/chunks/[name].[contenthash:8].css',
+        chunkFilename: dev
+          ? 'static/chunks/[name].chunk.css'
+          : 'static/chunks/[name].[contenthash:8].chunk.css',
+      })
+    );
+
+    config.plugins.push(
+      new webpack.DefinePlugin({
+        'process.env.NEXT_ENV': JSON.stringify(process.env.NEXT_ENV),
+        'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV),
+      })
+    );
+
+    return config;
+  },
+});
+
+export default {
   next: {
     dir: './client',
     dev: true,
@@ -15,5 +118,3 @@ const config = {
     }
   }
 };
-
-export default config;
