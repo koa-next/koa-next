b4931d337aa375300794a885c17b61dda950de70
PLDaily <pcd12321@gmail.com>
2019-02-24 16:57:06 +0800

    fix: fix jest did not exit one second after the test run has completed

--- test/app/service/home.test.ts
+++ test/app/service/home.test.ts
@@ -1,8 +1,9 @@
 import { home } from '../../../app/service';
 
 describe('test/app/service/home.test.ts', () => {
-  test('sayHi', async () => {
+  test('sayHi', async (done) => {
     const result = await home.sayHi('koa-next');
     expect(result).toBe('hi, koa-next');
+    done();
   });
 });
