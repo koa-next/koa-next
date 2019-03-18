69d8df7d7409e09fda6e40d6d9143c6a934a220f
PLDaily <pcd12321@gmail.com>
2019-03-18 17:37:16 +0800

    feat: support ssr cache

--- app.ts
+++ app.ts
@@ -1,16 +1,25 @@
 import * as Koa from 'koa';
 import * as log4js from 'koa-log4';
 import * as Next from 'next';
+import * as LRUCache from 'lru-cache';
+import NextMiddleware from 'koa-next-middleware';
 import config from './config';
 import router from './app/router';
 import logger from './app/middleware/logger';
-import next from 'koa-next-middleware';
 
 const app = new Koa();
 
+const isDev = process.env.NODE_ENV !== 'pro';
+const ssrCache = new LRUCache({
+  max: 100,
+  maxAge: 1000 * 60 * 60 // 1hour
+});
+
 if (config.next) {
   const appnext = Next(config.next);
-  app.use(next(appnext));
+  app.use(NextMiddleware(appnext, {
+    cache: isDev ? null : ssrCache
+  }));
 }
 
 log4js.configure(config.log4);
