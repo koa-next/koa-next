ea55e0fbcca38eb6b195c4aafde038b87bd10d52
PLDaily <pcd12321@gmail.com>
2019-09-04 19:13:25 +0800

    feat: upgrade

--- client/pages/_app.tsx
+++ client/pages/_app.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Provider } from 'react-redux';
 import withRedux from 'next-redux-wrapper';
-import App, { Container } from 'next/app';
+import App from 'next/app';
 import configureStore from '../redux';
 import { setLocales } from '../redux/modules/common';
 import { getLocales } from '../utils/locale';
@@ -41,11 +41,9 @@ class MyApp extends App<any> {
   render() {
     const { Component, pageProps, store } = this.props;
     return (
-      <Container>
-        <Provider store={store}>
-          <Component {...pageProps} />
-        </Provider>
-      </Container>
+      <Provider store={store}>
+        <Component {...pageProps} />
+      </Provider>
     );
   }
 }
